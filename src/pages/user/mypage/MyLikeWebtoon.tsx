3e118c4458651d2b58c6a7b2441cd9ddf0227bc0
손소원 <your@email.com>
2025-04-01 11:03:31 +0900

    feat: 마이페이지 유저페이지 , ui ux구성 초본

---
+++ src/pages/user/mypage/MyLikeWebtoon.tsx
@@ -0,0 +1,12 @@
+// src/pages/user/mypage/MyLikeWebtoon.tsx
+
+const MyLikeWebtoon = () => {
+  return (
+    <div>
+      <h1>내가 좋아요한 웹툰</h1>
+      {/* 여기에 목록 표시 컴포넌트들 추가 예정 */}
+    </div>
+  );
+};
+
+export default MyLikeWebtoon;
---
+++ src/pages/user/userpage/UserLikeWebtoon.tsx
@@ -0,0 +1,5 @@
+const UserLikeWebtoon = () => {
+  return <div>이 유저가 좋아요한 웹툰</div>;
+};
+
+export default UserLikeWebtoon;
