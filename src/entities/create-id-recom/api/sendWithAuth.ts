fd2bb3a2673b29a779ee2c7365b195612988d271
ta1355 <eorbs255@naver.com>
2025-04-15 12:37:19 +0900

    fix: api 백엔드와 맞게 수정

--- src/entities/create-id-recom/api/sendWithAuth.ts
+++ src/entities/create-id-recom/api/sendWithAuth.ts
@@ -1,9 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const sendWebtoonLogToSpring = async (webtoonIds: number[], token: string) => {
+export const sendWebtoonLogToSpring = async (
+  webtoonIds: number[],
+  token: string
+) => {
   const response = await axios.post(
-    'http://localhost:8080/api/logs/recommend',
-    { webtoon_ids: webtoonIds },
+    "http://localhost:8080/api/recommend/init",
+    { webtoonIds: webtoonIds },
     {
       headers: {
         Authorization: `Bearer ${token}`,
